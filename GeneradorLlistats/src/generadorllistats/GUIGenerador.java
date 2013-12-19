91c51f75bcb10032a3aedd03d8e8f9bb6dda0f54
Dani <Dani@infobosccoma.net>
2013-12-19 21:40:03 +0100

    Dani: Main inutil eliminat. XML funciona però només crea un alumne de cada matéria seleccionada

--- GeneradorLlistats/src/generadorllistats/GUIGenerador.java
+++ GeneradorLlistats/src/generadorllistats/GUIGenerador.java
@@ -8,30 +8,39 @@ package generadorllistats;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.SortedMap;
 import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  *
  * @author Roig
  */
-public class GUIGenerador extends javax.swing.JFrame{
+public class GUIGenerador extends javax.swing.JFrame {
 
     /**
      * Creates new form NewJFrame
      */
-    static llistaAlumnes llistaAlumnes = new llistaAlumnes();
-    static SortedMap<Assignatura, Integer> llistaCurtaAssignatures = new TreeMap<>();
+    List<Alumne> llistaAlumnes = new ArrayList<>();
+    SortedMap<Assignatura, Integer> llistaCurtaAssignatures = new TreeMap<>();
+
     public GUIGenerador() {
         initComponents();
     }
@@ -166,8 +175,8 @@ public class GUIGenerador extends javax.swing.JFrame{
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-      funcioPath();
-      crearAlumnes(path.getText());
+        funcioPath();
+        crearAlumnes(path.getText());
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
@@ -176,99 +185,119 @@ public class GUIGenerador extends javax.swing.JFrame{
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void pathActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pathActionPerformed
-           
+
     }//GEN-LAST:event_pathActionPerformed
     //Boto per generar el llistat
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-          if (jList1.getSelectedValuesList().isEmpty()){
-              JOptionPane.showMessageDialog(rootPane, "NO HI HA RES SELECCIONAT");
-          }else{
-             JOptionPane.showMessageDialog(rootPane, jList1.getSelectedValuesList());
-          }
+        if (jList1.getSelectedValuesList().isEmpty()) {
+            JOptionPane.showMessageDialog(rootPane, "NO HI HA RES SELECCIONAT");
+        } else {
+            generarXML();
+            JOptionPane.showMessageDialog(rootPane, "FITXER GENERAT CORRECTAMENT");
+        }
     }//GEN-LAST:event_jButton2ActionPerformed
 //Funcio que permet buscar un fitxer i escriure en el Text del costat
-    private void funcioPath(){
-   
-            //Creem un objecte fitxer del tipus FileChooser
-            JFileChooser fitxer = new JFileChooser();
-            fitxer.showOpenDialog(null);
-            //Creem un variable tipus Fitxer per obetnir el fitxer seleccionat anteriorment
-            File f = fitxer.getSelectedFile();
-            //Agafem el path i el guardem en una string
-            String nomFitxer = f.getAbsolutePath();
-            //Pintem el path dins el textbox anomenat 'path'"
-            if(f.getName().endsWith(".csv")){
-             path.setText(nomFitxer);
-            }else if(f.getName().endsWith(".csv") == false){
-                JOptionPane.showMessageDialog(null,"EL FITXER QUE HAS SELECCIONAT NO"
-             + " ES CORRECTE, SELECCIONA UN FITXER AMB EXTENSIÓ .CSV");
-         }
-    }
-    private void generarXML(List<Alumne> llistaAlumnes) {
-        try {
-            FileOutputStream file = new FileOutputStream("FitxerAlumnes.xml", false);
 
-            JAXBContext jaxbContext = JAXBContext.newInstance(Alumne.class);
-            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+    private void funcioPath() {
 
-            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-            jaxbMarshaller.marshal(llistaAlumnes, file);// envia a fitxer
-            jaxbMarshaller.marshal(llistaAlumnes, System.out);// envia a consola
-
-        } catch (JAXBException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        //Creem un objecte fitxer del tipus FileChooser
+        JFileChooser fitxer = new JFileChooser();
+        fitxer.showOpenDialog(null);
+        //Creem un variable tipus Fitxer per obetnir el fitxer seleccionat anteriorment
+        File f = fitxer.getSelectedFile();
+        //Agafem el path i el guardem en una string
+        String nomFitxer = f.getAbsolutePath();
+        //Pintem el path dins el textbox anomenat 'path'"
+        if (f.getName().endsWith(".csv")) {
+            path.setText(nomFitxer);
+        } else if (f.getName().endsWith(".csv") == false) {
+            JOptionPane.showMessageDialog(null, "EL FITXER QUE HAS SELECCIONAT NO"
+                    + " ES CORRECTE, SELECCIONA UN FITXER AMB EXTENSIÓ .CSV");
         }
     }
-    
-    private void crearAlumnes(String path){
+
+    private void generarXML() {
+        List<Assignatura> materiesSeleccionades = jList1.getSelectedValuesList();
+        try {        
+            DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document doc = (Document) docBuilder.newDocument();
+            Element rootElement = doc.createElement("llistes");
+            doc.appendChild(rootElement);
+
+            for (Assignatura str : materiesSeleccionades) {
+                Element assignatura = doc.createElement("llista");
+                assignatura.setAttribute("materia", str.getNom());
+                for (int i = 0; i < llistaAlumnes.size(); i++) {
+                    if (llistaAlumnes.get(i).getLlistAssign().contains(str)) {
+                        Element alumne = doc.createElement("alumne");
+                        Element nomCognom = doc.createElement("cognomsNom");
+                        nomCognom.appendChild(doc.createTextNode(llistaAlumnes.get(i).getNomComplet()));
+                        alumne.appendChild(nomCognom);
+                        Element curs = doc.createElement("grup");
+                        curs.appendChild(doc.createTextNode(llistaAlumnes.get(i).getCurs()));
+                        alumne.appendChild(curs);
+                        assignatura.appendChild(alumne);
+                    }
+                }
+                rootElement.appendChild(assignatura);
+            }
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File("llistaAlumesFiltrada.xml"));
+            transformer.transform(source, result);
+            
+        } catch (TransformerException | ParserConfigurationException ex) {
+            Logger.getLogger(GUIGenerador.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        
+    }
+
+    private void crearAlumnes(String path) {
         try {
-            llistaAlumnes.setAlumnes(new ArrayList<Alumne>());//llista on desarem tots els objectes Alumne
             BufferedReader reader = new BufferedReader(new FileReader(path));//Llegim el fitxer
             String line = "";
             String patro = "\" #,\"\"00_NOM\"\",\"\"01_GRUPSCLASSE\"\",\"\"02_MATRICULADES\"\"\"";
             line = reader.readLine();//Saltem la línia de títols
-            if(line.compareTo(patro) == 0){
+            if (line.compareTo(patro) == 0) {
                 while ((line = reader.readLine()) != null) {//mentre llegeixis linies del fitxer...
-                String[] dades = line.trim().split(",|\"");//desem els camps de la linia llegida separats per comes o cometes dobles
-                List<Assignatura> llistaAssignatures = new ArrayList<>();//Creem una llista de assignatures
-                 for (int x = 15; x < dades.length; x++) {//mentre hi hagi assignatures a la línia d'aquest Alumne
-                    Assignatura as = new Assignatura();//creem assignatura
-                    as.setNom(dades[x]);//li donem el nom de l'assignatura
-                    llistaAssignatures.add(as);//afegeix el nom de cada assignatura a la llista d'assignatures
+                    String[] dades = line.trim().split(",|\"");//desem els camps de la linia llegida separats per comes o cometes dobles
+                    List<Assignatura> llistaAssignatures = new ArrayList<>();//Creem una llista de assignatures
+                    for (int x = 15; x < dades.length; x++) {//mentre hi hagi assignatures a la línia d'aquest Alumne
+                        Assignatura as = new Assignatura();//creem assignatura
+                        as.setNom(dades[x]);//li donem el nom de l'assignatura
+                        llistaAssignatures.add(as);//afegeix el nom de cada assignatura a la llista d'assignatures
 
-                }
-              //////Saber quines assignatures hi ha en total sense repetir-les
-                    for (Assignatura aux : llistaAssignatures) {//per cada persona de l'array...
-                    if (llistaCurtaAssignatures.containsKey(aux)) {//si ia existeix a l'array
-                        llistaCurtaAssignatures.put(aux, llistaCurtaAssignatures.get(aux) + 1);//sumem +1 al valor de la persona valor(el valor es l'integer del hashmap
-                    } else {//si no existia
-                        llistaCurtaAssignatures.put(aux, 1);//afegim la persona al hashmap amb el valor 1
                     }
-                }
+                    //////Saber quines assignatures hi ha en total sense repetir-les
+                    for (Assignatura aux : llistaAssignatures) {//per cada persona de l'array...
+                        if (llistaCurtaAssignatures.containsKey(aux)) {//si ia existeix a l'array
+                            llistaCurtaAssignatures.put(aux, llistaCurtaAssignatures.get(aux) + 1);//sumem +1 al valor de la persona valor(el valor es l'integer del hashmap
+                        } else {//si no existia
+                            llistaCurtaAssignatures.put(aux, 1);//afegim la persona al hashmap amb el valor 1
+                        }
+                    }
               /////
-                
-                Alumne a = new Alumne();//creem l'alumne
-                a.setCodi(dades[1]);//afegim el codi
-                a.setNomComplet(dades[4] + "," + dades[5]);//afegim nom complet
-                a.setCurs(dades[10]);//afegim curs
-                a.setLlistAssign(llistaAssignatures);//afegim llista assignatures
-                llistaAlumnes.getAlumnes().add(a);//afegim l'alumne amb les seves dades a la llista d'alumnes
+
+                    Alumne a = new Alumne();//creem l'alumne
+                    a.setCodi(dades[1]);//afegim el codi
+                    a.setNomComplet(dades[4] + "," + dades[5]);//afegim nom complet
+                    a.setCurs(dades[10]);//afegim curs
+                    a.setLlistAssign(llistaAssignatures);//afegim llista assignatures
+                    llistaAlumnes.add(a);//afegim l'alumne amb les seves dades a la llista d'alumnes
+                }
+
+                jList1.setListData(llistaCurtaAssignatures.keySet().toArray());
+            } else {
+                JOptionPane.showMessageDialog(null, "EL FORMAT DEL FITXER .CSV NO ÉS CORRECTE!!!");
             }
-            
-            jList1.setListData(llistaCurtaAssignatures.keySet().toArray());
-        }else{
-                JOptionPane.showMessageDialog(null,"EL FORMAT DEL FITXER .CSV NO ÉS CORRECTE!!!");
-             }
-          
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
+
     }
 
     /**
